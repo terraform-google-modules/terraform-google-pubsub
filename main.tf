ec3501cd80ab22e5967fa062a65462f2780e9c99
Kunpei Sakai <kunpei@google.com>
2018-10-22 16:42:31 +0900

    implement terraform-google-pubsub module

---
+++ main.tf
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+locals {
+  default_ack_deadline_seconds = 10
+}
+
+resource "google_pubsub_topic" "topic" {
+  name    = "${var.topic}"
+  project = "${var.project_id}"
+}
+
+resource "google_pubsub_subscription" "push_subscriptions" {
+  count                = "${length(var.push_subscriptions)}"
+  name                 = "${lookup(var.push_subscriptions[count.index], "name")}"
+  topic                = "${var.topic}"
+  project              = "${var.project_id}"
+  ack_deadline_seconds = "${lookup(var.push_subscriptions[count.index], "ack_deadline_seconds", local.default_ack_deadline_seconds)}"
+
+  push_config {
+    push_endpoint = "${lookup(var.push_subscriptions[count.index], "push_endpoint")}"
+  }
+
+  depends_on = ["google_pubsub_topic.topic"]
+}
+
+resource "google_pubsub_subscription" "pull_subscriptions" {
+  count                = "${length(var.pull_subscriptions)}"
+  name                 = "${lookup(var.pull_subscriptions[count.index], "name")}"
+  topic                = "${var.topic}"
+  project              = "${var.project_id}"
+  ack_deadline_seconds = "${lookup(var.pull_subscriptions[count.index], "ack_deadline_seconds", local.default_ack_deadline_seconds)}"
+
+  depends_on = ["google_pubsub_topic.topic"]
+}
---
+++ test/fixtures/main.tf
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+provider "google" {
+  credentials = "${file(var.credentials_file_path)}"
+}
+
+module "pubsub" {
+  source     = "../../"
+  project_id = "${var.project}"
+  topic      = "${var.topic_name}"
+
+  push_subscriptions = [
+    {
+      name                 = "push"
+      push_endpoint        = "https://${var.project}.appspot.com/"
+      ack_deadline_seconds = 20
+    },
+  ]
+
+  pull_subscriptions = [
+    {
+      name = "pull"
+    },
+  ]
+}
