c808032ba6ad5046407c771113555c9feec95007
Kunpei Sakai <kunpei@google.com>
2018-10-25 02:02:46 +0900

    use string instead of list

--- outputs.tf
+++ outputs.tf
@@ -15,7 +15,7 @@
  */
 
 output "topic" {
-  value       = ["${google_pubsub_topic.topic.name}"]
+  value       = "${google_pubsub_topic.topic.name}"
   description = "The name of the Pub/Sub topic"
 }
 
