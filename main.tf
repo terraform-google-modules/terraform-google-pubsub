16171c6d6773c1caf3bb510b7646c93d7ffda28d
Kunpei Sakai <kunpei@google.com>
2018-10-26 08:43:34 +0900

    use google_pubsub_topic.topic.name instead of var.topic

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "google_pubsub_topic" "topic" {
 resource "google_pubsub_subscription" "push_subscriptions" {
   count                = "${length(var.push_subscriptions)}"
   name                 = "${lookup(var.push_subscriptions[count.index], "name")}"
-  topic                = "${var.topic}"
+  topic                = "${google_pubsub_topic.topic.name}"
   project              = "${var.project_id}"
   ack_deadline_seconds = "${lookup(var.push_subscriptions[count.index], "ack_deadline_seconds", local.default_ack_deadline_seconds)}"
 
@@ -40,7 +40,7 @@ resource "google_pubsub_subscription" "push_subscriptions" {
 resource "google_pubsub_subscription" "pull_subscriptions" {
   count                = "${length(var.pull_subscriptions)}"
   name                 = "${lookup(var.pull_subscriptions[count.index], "name")}"
-  topic                = "${var.topic}"
+  topic                = "${google_pubsub_topic.topic.name}"
   project              = "${var.project_id}"
   ack_deadline_seconds = "${lookup(var.pull_subscriptions[count.index], "ack_deadline_seconds", local.default_ack_deadline_seconds)}"
 
