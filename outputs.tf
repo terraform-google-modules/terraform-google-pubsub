4cbcc5e878c1d184672bea6bfb73e4b7f2831737
Kunpei Sakai <kunpei@google.com>
2018-10-26 08:44:35 +0900

    expose subscription paths, use the concat func explicitly

--- outputs.tf
+++ outputs.tf
@@ -19,11 +19,20 @@ output "topic" {
   description = "The name of the Pub/Sub topic"
 }
 
-output "subscriptions" {
-  value = [
-    "${google_pubsub_subscription.push_subscriptions.*.name}",
-    "${google_pubsub_subscription.pull_subscriptions.*.name}",
-  ]
+output "subscription_names" {
+  value = "${concat(
+    google_pubsub_subscription.push_subscriptions.*.name,
+    google_pubsub_subscription.pull_subscriptions.*.name
+  )}"
 
   description = "The name list of Pub/Sub subscriptions"
+}
+
+output "subscription_paths" {
+  value = "${concat(
+    google_pubsub_subscription.push_subscriptions.*.path,
+    google_pubsub_subscription.pull_subscriptions.*.path
+  )}"
+
+  description = "The path list of Pub/Sub subscriptions"
 }
