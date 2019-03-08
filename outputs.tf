db89a5c248c3bb621e0ee60a4714d20fe3fcb49e
Kunpei Sakai <kunpei@google.com>
2019-03-09 01:03:11 +0900

    add uri as an output resource

--- outputs.tf
+++ outputs.tf
@@ -24,6 +24,11 @@ output "id" {
   description = "The ID of the Pub/Sub topic"
 }
 
+output "uri" {
+  value       = "pubsub.googleapis.com/${google_pubsub_topic.topic.id}"
+  description = "The URI of the Pub/Sub topic"
+}
+
 output "subscription_names" {
   value = "${concat(
     google_pubsub_subscription.push_subscriptions.*.name,
