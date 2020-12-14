bc07fba06018de14e32b9aa48a6311db627556b4
Anastasiia Kovtun <nastia@Anastasiias-MacBook-Pro-2.local>
2020-12-14 12:22:48 +0200

    Add grant_token_creator variable

--- variables.tf
+++ variables.tf
@@ -59,3 +59,9 @@ variable "topic_kms_key_name" {
   description = "The resource name of the Cloud KMS CryptoKey to be used to protect access to messages published on this topic."
   default     = null
 }
+
+variable "grant_token_creator" {
+  type        = bool
+  description = "Specify true if you want to add token creator role to the default Pub/Sub SA"
+  default     = false
+}
