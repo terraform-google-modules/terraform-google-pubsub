804c707a5e12af82618299a399e24ea20dbaa956
Mackenzie Clark <mackenzieclark@google.com>
2022-07-12 21:31:48 +0000

    Re-adding suggested versions.tf changes

--- examples/cloudiot/versions.tf
+++ examples/cloudiot/versions.tf
@@ -18,7 +18,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.1.0"
+      version = "~> 4.1"
     }
     tls = {
       source = "hashicorp/tls"
