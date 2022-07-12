af513500750b4262b9229a9b69afcb972701ea54
Mackenzie Clark <38470431+macksclark@users.noreply.github.com>
2022-07-12 16:45:36 -0400

    Apply suggestions from code review
    Co-authored-by: Andrew Peabody <apeabody@gmail.com>

--- examples/simple/versions.tf
+++ examples/simple/versions.tf
@@ -18,7 +18,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.1.0"
+      version = "~> 4.1"
     }
   }
   required_version = ">= 0.13"
