5954c662b82e019eec8d4744031f7405d37841f6
Mackenzie Clark <mackenzieclark@google.com>
2022-07-12 21:30:08 +0000

    Revert "Apply suggestions from code review"
    This reverts commit af513500750b4262b9229a9b69afcb972701ea54.

--- examples/kms/versions.tf
+++ examples/kms/versions.tf
@@ -18,7 +18,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.1"
+      version = "~> 4.1.0"
     }
   }
   required_version = ">= 0.13"
