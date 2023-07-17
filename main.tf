e09ac95ddaa78b0608f0f91d95d5353a0b21af1f
Casey O'Hare <ORabbit@users.noreply.github.com>
2023-07-17 16:26:53 -0400

    Add support for push payload unwrapping (no wrapper)

--- main.tf
+++ main.tf
@@ -197,13 +197,13 @@ resource "google_pubsub_subscription" "push_subscriptions" {
     }
 
     dynamic "pubsub_wrapper" {
-      for_each = (lookup(each.value, "pubsub_wrapper", "") != "") ? [true] : []
+      for_each = lookup(each.value, "pubsub_wrapper", "") ? [true] : []
     }
 
     dynamic "no_wrapper" {
-      for_each = (lookup(each.value, "no_wrapper", "") != "") ? [true] : []
+      for_each = lookup(each.value, "no_wrapper", "") ? [true] : []
       content {
-        write_metadata = lookup(each.value, "write_metadata", null)
+        write_metadata = lookup(each.value, "no_wrapper_write_metadata", null)
       }
     }
   }
