2bdbea6aeff1315a1aa8a6b52e7f7a8e82a54ec8
Awais Malik <awmalik@google.com>
2023-10-20 10:12:01 -0700

    fix: upgraded versions.tf to include minor bumps from tpg v5 (#153)
    Co-authored-by: Andrew Peabody <andrewpeabody@google.com>

--- test/setup/versions.tf
+++ test/setup/versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.23"
+      version = ">= 4.23"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4.23"
+      version = ">= 4.23"
     }
     null = {
       source = "hashicorp/null"
