terraform-google-modules/terraform-google-pubsub

commit f23924757405313c452a9bd94f888a09b1093636

fix: migrate pull_subscriptions and push_subscriptions to object lists (#224)

metadata.yaml
@@ -65,11 +65,48 @@ spec:
         defaultValue: {}
       - name: push_subscriptions
         description: The list of the push subscriptions.
-        varType: list(map(string))
+        varType: |-
+          list(object({
+              name                       = string,
+              ack_deadline_seconds       = optional(number),
+              push_endpoint              = optional(string),
+              x-goog-version             = optional(string),
+              oidc_service_account_email = optional(string),
+              audience                   = optional(string),
+              expiration_policy          = optional(string),
+              dead_letter_topic          = optional(string),
+              retain_acked_messages      = optional(bool),
+              message_retention_duration = optional(string),
+              max_delivery_attempts      = optional(number),
+              maximum_backoff            = optional(string),
+              minimum_backoff            = optional(string),
+              filter                     = optional(string),
+              enable_message_ordering    = optional(bool),
+            }))
         defaultValue: []
+        connections:
+          - source:
+              source: github.com/GoogleCloudPlatform/terraform-google-cloud-run//modules/v2
+              version: ">= 0.13"
+            spec:
+              outputExpr: "{ \"name\": service_name, \"push_endpoint\": service_uri, \"oidc_service_account_email\": service_account_id.email }"
       - name: pull_subscriptions
         description: The list of the pull subscriptions.
-        varType: list(map(string))
+        varType: |-
+          list(object({
+              name                         = string,
+              ack_deadline_seconds         = optional(number),
+              dead_letter_topic            = optional(string),
+              max_delivery_attempts        = optional(number),
+              retain_acked_messages        = optional(bool),
+              message_retention_duration   = optional(string),
+              maximum_backoff              = optional(number),
+              minimum_backoff              = optional(number),
+              filter                       = optional(string),
+              enable_message_ordering      = optional(bool),
+              service_account              = optional(string),
+              enable_exactly_once_delivery = optional(bool),
+            }))
         defaultValue: []
       - name: bigquery_subscriptions
         description: The list of the Bigquery push subscriptions.
@@ -97,8 +134,7 @@ spec:
               source: github.com/terraform-google-modules/terraform-google-bigquery
               version: ">= 10.0.0"
             spec:
-              outputExpr: external_table_ids[0]
-              inputPath: table
+              outputExpr: "{ \"name\": external_table_ids[0], \"table\": external_table_ids[0]}"
       - name: cloud_storage_subscriptions
         description: The list of the Cloud Storage push subscriptions.
         varType: |-
@@ -130,8 +166,7 @@ spec:
               source: github.com/terraform-google-modules/terraform-google-cloud-storage//modules/simple_bucket
               version: ">= 9.0.1"
             spec:
-              outputExpr: name
-              inputPath: bucket
+              outputExpr: "{ \"name\": name, \"bucket\": name}"
       - name: subscription_labels
         description: A map of labels to assign to every Pub/Sub subscription.
         varType: map(string)

variables.tf
@@ -42,13 +42,42 @@ variable "topic_labels" {
 }
 
 variable "push_subscriptions" {
-  type        = list(map(string))
+  type = list(object({
+    name                       = string,
+    ack_deadline_seconds       = optional(number),
+    push_endpoint              = optional(string),
+    x-goog-version             = optional(string),
+    oidc_service_account_email = optional(string),
+    audience                   = optional(string),
+    expiration_policy          = optional(string),
+    dead_letter_topic          = optional(string),
+    retain_acked_messages      = optional(bool),
+    message_retention_duration = optional(string),
+    max_delivery_attempts      = optional(number),
+    maximum_backoff            = optional(string),
+    minimum_backoff            = optional(string),
+    filter                     = optional(string),
+    enable_message_ordering    = optional(bool),
+  }))
   description = "The list of the push subscriptions."
   default     = []
 }
 
 variable "pull_subscriptions" {
-  type        = list(map(string))
+  type = list(object({
+    name                         = string,
+    ack_deadline_seconds         = optional(number),
+    dead_letter_topic            = optional(string),
+    max_delivery_attempts        = optional(number),
+    retain_acked_messages        = optional(bool),
+    message_retention_duration   = optional(string),
+    maximum_backoff              = optional(number),
+    minimum_backoff              = optional(number),
+    filter                       = optional(string),
+    enable_message_ordering      = optional(bool),
+    service_account              = optional(string),
+    enable_exactly_once_delivery = optional(bool),
+  }))
   description = "The list of the pull subscriptions."
   default     = []
 }

main.tf
@@ -63,7 +63,7 @@ resource "google_project_iam_member" "token_creator_binding" {
 }
 
 resource "google_pubsub_topic_iam_member" "push_topic_binding" {
-  for_each = var.create_topic ? { for i in var.push_subscriptions : i.name => i if try(i.dead_letter_topic, "") != "" } : {}
+  for_each = var.create_topic ? { for i in var.push_subscriptions : i.name => i if i.dead_letter_topic != null } : {}
 
   project = var.project_id
   topic   = each.value.dead_letter_topic
@@ -75,7 +75,7 @@ resource "google_pubsub_topic_iam_member" "push_topic_binding" {
 }
 
 resource "google_pubsub_topic_iam_member" "pull_topic_binding" {
-  for_each = var.create_topic ? { for i in var.pull_subscriptions : i.name => i if try(i.dead_letter_topic, "") != "" } : {}
+  for_each = var.create_topic ? { for i in var.pull_subscriptions : i.name => i if i.dead_letter_topic != null } : {}
 
   project = var.project_id
   topic   = each.value.dead_letter_topic
@@ -99,7 +99,7 @@ resource "google_pubsub_topic_iam_member" "bigquery_topic_binding" {
 }
 
 resource "google_pubsub_subscription_iam_member" "pull_subscription_binding" {
-  for_each = var.create_subscriptions ? { for i in var.pull_subscriptions : i.name => i if try(i.dead_letter_topic, "") != "" } : {}
+  for_each = var.create_subscriptions ? { for i in var.pull_subscriptions : i.name => i if i.dead_letter_topic != null } : {}
 
   project      = var.project_id
   subscription = each.value.name
@@ -115,7 +115,7 @@ resource "google_pubsub_subscription_iam_member" "pull_subscription_binding" {
 }
 
 resource "google_pubsub_subscription_iam_member" "push_subscription_binding" {
-  for_each = var.create_subscriptions ? { for i in var.push_subscriptions : i.name => i if try(i.dead_letter_topic, "") != "" } : {}
+  for_each = var.create_subscriptions ? { for i in var.push_subscriptions : i.name => i if i.dead_letter_topic != null } : {}
 
   project      = var.project_id
   subscription = each.value.name
@@ -170,35 +170,15 @@ resource "google_pubsub_topic" "topic" {
 resource "google_pubsub_subscription" "push_subscriptions" {
   for_each = var.create_subscriptions ? { for i in var.push_subscriptions : i.name => i } : {}
 
-  name    = each.value.name
-  topic   = var.create_topic ? google_pubsub_topic.topic[0].name : var.topic
-  project = var.project_id
-  labels  = var.subscription_labels
-  ack_deadline_seconds = lookup(
-    each.value,
-    "ack_deadline_seconds",
-    local.default_ack_deadline_seconds,
-  )
-  message_retention_duration = lookup(
-    each.value,
-    "message_retention_duration",
-    null,
-  )
-  retain_acked_messages = lookup(
-    each.value,
-    "retain_acked_messages",
-    null,
-  )
-  filter = lookup(
-    each.value,
-    "filter",
-    null,
-  )
-  enable_message_ordering = lookup(
-    each.value,
-    "enable_message_ordering",
-    null,
-  )
+  name                       = each.value.name
+  topic                      = var.create_topic ? google_pubsub_topic.topic[0].name : var.topic
+  project                    = var.project_id
+  labels                     = var.subscription_labels
+  ack_deadline_seconds       = each.value.ack_deadline_seconds != null ? each.value.ack_deadline_seconds : local.default_ack_deadline_seconds
+  message_retention_duration = each.value.message_retention_duration
+  retain_acked_messages      = each.value.retain_acked_messages
+  filter                     = each.value.filter
+  enable_message_ordering    = each.value.enable_message_ordering
   dynamic "expiration_policy" {
     // check if the 'expiration_policy' key exists, if yes, return a list containing it.
     for_each = contains(keys(each.value), "expiration_policy") ? [each.value.expiration_policy] : []
@@ -208,18 +188,18 @@ resource "google_pubsub_subscription" "push_subscriptions" {
   }
 
   dynamic "dead_letter_policy" {
-    for_each = (lookup(each.value, "dead_letter_topic", "") != "") ? [each.value.dead_letter_topic] : []
+    for_each = each.value.dead_letter_topic != null ? [each.value.dead_letter_topic] : []
     content {
-      dead_letter_topic     = lookup(each.value, "dead_letter_topic", "")
-      max_delivery_attempts = lookup(each.value, "max_delivery_attempts", "5")
+      dead_letter_topic     = each.value.dead_letter_topic != null ? each.value.dead_letter_topic : ""
+      max_delivery_attempts = each.value.max_delivery_attempts != null ? each.value.max_delivery_attempts : ""
     }
   }
 
   dynamic "retry_policy" {
-    for_each = (lookup(each.value, "maximum_backoff", "") != "") ? [each.value.maximum_backoff] : []
+    for_each = each.value.maximum_backoff != null ? [each.value.maximum_backoff] : []
     content {
-      maximum_backoff = lookup(each.value, "maximum_backoff", "")
-      minimum_backoff = lookup(each.value, "minimum_backoff", "")
+      maximum_backoff = each.value.maximum_backoff != null ? each.value.maximum_backoff : ""
+      minimum_backoff = each.value.minimum_backoff != null ? each.value.minimum_backoff : "5"
     }
   }
 
@@ -229,14 +209,14 @@ resource "google_pubsub_subscription" "push_subscriptions" {
     // FIXME: This should be programmable, but nested map isn't supported at this time.
     //   https://github.com/hashicorp/terraform/issues/2114
     attributes = {
-      x-goog-version = lookup(each.value, "x-goog-version", "v1")
+      x-goog-version = each.value.minimum_backoff != null ? "x-goog-version" : "v1"
     }
 
     dynamic "oidc_token" {
-      for_each = (lookup(each.value, "oidc_service_account_email", "") != "") ? [true] : []
+      for_each = each.value.oidc_service_account_email != null ? [true] : []
       content {
-        service_account_email = lookup(each.value, "oidc_service_account_email", "")
-        audience              = lookup(each.value, "audience", "")
+        service_account_email = each.value.oidc_service_account_email != null ? each.value.oidc_service_account_email : ""
+        audience              = each.value.audience != null ? each.value.audience : ""
       }
     }
   }
@@ -248,40 +228,16 @@ resource "google_pubsub_subscription" "push_subscriptions" {
 resource "google_pubsub_subscription" "pull_subscriptions" {
   for_each = var.create_subscriptions ? { for i in var.pull_subscriptions : i.name => i } : {}
 
-  name    = each.value.name
-  topic   = var.create_topic ? google_pubsub_topic.topic[0].name : var.topic
-  project = var.project_id
-  labels  = var.subscription_labels
-  enable_exactly_once_delivery = lookup(
-    each.value,
-    "enable_exactly_once_delivery",
-    null,
-  )
-  ack_deadline_seconds = lookup(
-    each.value,
-    "ack_deadline_seconds",
-    local.default_ack_deadline_seconds,
-  )
-  message_retention_duration = lookup(
-    each.value,
-    "message_retention_duration",
-    null,
-  )
-  retain_acked_messages = lookup(
-    each.value,
-    "retain_acked_messages",
-    null,
-  )
-  filter = lookup(
-    each.value,
-    "filter",
-    null,
-  )
-  enable_message_ordering = lookup(
-    each.value,
-    "enable_message_ordering",
-    null,
-  )
+  name                         = each.value.name
+  topic                        = var.create_topic ? google_pubsub_topic.topic[0].name : var.topic
+  project                      = var.project_id
+  labels                       = var.subscription_labels
+  enable_exactly_once_delivery = each.value.enable_exactly_once_delivery
+  ack_deadline_seconds         = each.value.ack_deadline_seconds != null ? each.value.ack_deadline_seconds : local.default_ack_deadline_seconds
+  message_retention_duration   = each.value.message_retention_duration
+  retain_acked_messages        = each.value.retain_acked_messages
+  filter                       = each.value.filter
+  enable_message_ordering      = each.value.enable_message_ordering
   dynamic "expiration_policy" {
     // check if the 'expiration_policy' key exists, if yes, return a list containing it.
     for_each = contains(keys(each.value), "expiration_policy") ? [each.value.expiration_policy] : []
@@ -291,18 +247,18 @@ resource "google_pubsub_subscription" "pull_subscriptions" {
   }
 
   dynamic "dead_letter_policy" {
-    for_each = (lookup(each.value, "dead_letter_topic", "") != "") ? [each.value.dead_letter_topic] : []
+    for_each = each.value.dead_letter_topic != null ? [each.value.dead_letter_topic] : []
     content {
-      dead_letter_topic     = lookup(each.value, "dead_letter_topic", "")
-      max_delivery_attempts = lookup(each.value, "max_delivery_attempts", "5")
+      dead_letter_topic     = each.value.dead_letter_topic != null ? each.value.dead_letter_topic : ""
+      max_delivery_attempts = each.value.max_delivery_attempts != null ? each.value.max_delivery_attempts : "5"
     }
   }
 
   dynamic "retry_policy" {
-    for_each = (lookup(each.value, "maximum_backoff", "") != "") ? [each.value.maximum_backoff] : []
+    for_each = each.value.maximum_backoff != null ? [each.value.maximum_backoff] : []
     content {
-      maximum_backoff = lookup(each.value, "maximum_backoff", "")
-      minimum_backoff = lookup(each.value, "minimum_backoff", "")
+      maximum_backoff = each.value.maximum_backoff != null ? each.value.maximum_backoff : ""
+      minimum_backoff = each.value.minimum_backoff != null ? each.value.minimum_backoff : "5"
     }
   }
 
@@ -422,7 +378,7 @@ resource "google_pubsub_subscription" "cloud_storage_subscriptions" {
 }
 
 resource "google_pubsub_subscription_iam_member" "pull_subscription_sa_binding_subscriber" {
-  for_each = var.create_subscriptions ? { for i in var.pull_subscriptions : i.name => i if lookup(i, "service_account", null) != null } : {}
+  for_each = var.create_subscriptions ? { for i in var.pull_subscriptions : i.name => i if i.service_account != null } : {}
 
   project      = var.project_id
   subscription = each.value.name
@@ -438,7 +394,7 @@ resource "google_pubsub_subscription_iam_member" "pull_subscription_sa_binding_s
 }
 
 resource "google_pubsub_subscription_iam_member" "pull_subscription_sa_binding_viewer" {
-  for_each = var.create_subscriptions ? { for i in var.pull_subscriptions : i.name => i if lookup(i, "service_account", null) != null } : {}
+  for_each = var.create_subscriptions ? { for i in var.pull_subscriptions : i.name => i if i.service_account != null } : {}
 
   project      = var.project_id
   subscription = each.value.name

commit 30c2a105e0efdde7247468cf636a70dcbb3f66c1

fix: set type of maximum_backoff and minimum_backoff to string in pull_subscriptions
Fixes regression introduced in #224
Fixes #227

metadata.yaml
@@ -27,7 +27,7 @@ spec:
     version: 7.0.0
     actuationTool:
       flavor: Terraform
-      version: ">= 0.13"
+      version: ">= 1.3"
     description: {}
   content:
     examples:
@@ -100,8 +100,8 @@ spec:
               max_delivery_attempts        = optional(number),
               retain_acked_messages        = optional(bool),
               message_retention_duration   = optional(string),
-              maximum_backoff              = optional(number),
-              minimum_backoff              = optional(number),
+              maximum_backoff              = optional(string),
+              minimum_backoff              = optional(string),
               filter                       = optional(string),
               enable_message_ordering      = optional(bool),
               service_account              = optional(string),

variables.tf
@@ -71,8 +71,8 @@ variable "pull_subscriptions" {
     max_delivery_attempts        = optional(number),
     retain_acked_messages        = optional(bool),
     message_retention_duration   = optional(string),
-    maximum_backoff              = optional(number),
-    minimum_backoff              = optional(number),
+    maximum_backoff              = optional(string),
+    minimum_backoff              = optional(string),
     filter                       = optional(string),
     enable_message_ordering      = optional(bool),
     service_account              = optional(string),